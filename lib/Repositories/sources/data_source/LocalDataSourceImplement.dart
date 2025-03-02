353ba04946dbcf6a91c387fc9f708bfe73969daa
salmaehab26 <zalmaehab2002@gmail.com>
2025-03-02 03:14:36 +0200

    fixed error

--- lib/Repositories/sources/data_source/LocalDataSourceImplement.dart
+++ lib/Repositories/sources/data_source/LocalDataSourceImplement.dart
@@ -8,22 +8,39 @@ import '../SourceDataSource.dart';
 class LocalDataSourceImple implements SourceLocalDataSource {
 
   @override
-  Future<SourceResponse?> getSources(String categoryId)  async {
-    var box=await Hive.openBox("tabs");
+
+  Future<SourceResponse?> getSources(String categoryId) async {
+    var box = await Hive.openBox("tabs");
     var jsonData = box.get(categoryId);
+
+    if (jsonData == null) {
+      print("No data found for category: $categoryId");
+      return null;
+    }
+
     print("Retrieved data from Hive: $jsonData"); // Debugging line
-    var data = SourceResponse.fromJson(json.decode(jsonData));
-    print(" data from Hive: $jsonData"); // Debugging line
-    return data;
 
+    try {
+      if (jsonData is Map) {
+        var data = jsonData.map((key, value) => MapEntry(key.toString(), value));
+        return SourceResponse.fromJson(data);
+      } else if (jsonData is String) {
+        var decodedData = json.decode(jsonData) as Map<String, dynamic>;
+        return SourceResponse.fromJson(decodedData);
+      }
+    } catch (e) {
+      print("Error processing data from Hive: $e");
+    }
 
-
+    print("Unexpected data type in Hive: ${jsonData.runtimeType}");
+    return null;
   }
   @override
   void SaveSources(SourceResponse? sourceResponse, String category) async {
-   var box=await Hive.openBox("tabs");
-   await box.put(category, sourceResponse?.toJson());
-   await box.close();
+    var box=await Hive.openBox("tabs");
+    await box.put(category, json.encode(sourceResponse?.toJson()));
+    await box.close();
+
   }
 
 }
